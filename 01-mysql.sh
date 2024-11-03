57e5abb749a547aaf453de41e5718c6963d82799
Sunil <sunil.nunubilli@gmail.com>
2024-11-03 17:10:34 +0530

    expense-shell

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -1,5 +1,5 @@
 LOGPATH="/var/log/expense-shell/"
-SCRIPTNAME=$(ech0 $0 | cut -d "." -f1)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 FILENAME="$LOGPATH/$SCRIPTNAME-$TIMESTAMP.log"
 
