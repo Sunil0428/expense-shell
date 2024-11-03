2eef084e1a5bf1d3e343842b7b327402878efca5
Sunil <sunil.nunubilli@gmail.com>
2024-11-03 19:20:39 +0530

    expense-shell

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -7,21 +7,20 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-CHECKUSER{
+CHECKUSER()
     if [ $1 -ne 0 ]
     then
         echo "Please use the root user access to run this script"
         exit 1
     fi
-}
-VALIDATE{
+
+VALIDATE()
     if [ $1 -eq 0 ]
     then
         echo -e "$2 is $G SUCCESS $N" | tee -a FILENAME
     else
         echo -e  "$2 is $R FAILED $N" | tee -a FILENAME
     fi
-}
 
 USERID=$(id -u)
 
