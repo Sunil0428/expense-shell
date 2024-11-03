1d35136f8fffe5b25e9ad28646f19e1bb932b017
Sunil <sunil.nunubilli@gmail.com>
2024-11-03 19:18:59 +0530

    expense-shell

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -3,4 +3,53 @@ SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 FILENAME="$LOGPATH/$SCRIPTNAME-$TIMESTAMP.log"
 
-echo $FILENAME
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECKUSER{
+    if [ $1 -ne 0 ]
+    then
+        echo "Please use the root user access to run this script"
+        exit 1
+    fi
+}
+VALIDATE{
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is $G SUCCESS $N" | tee -a FILENAME
+    else
+        echo -e  "$2 is $R FAILED $N" | tee -a FILENAME
+    fi
+}
+
+USERID=$(id -u)
+
+CHECKUSER $USERID
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo -e "$R mysql is not there in the system, hence installing $N" | tee -a FILENAME
+    dnf install mysql -y &>>FILENAME
+    VALIDATE $? "Installing mysql"
+fi
+
+systemctl enable mysqld | tee -a FILENAME
+VALIDATE $? "enabling mysql"
+
+systemctl start mysqld | tee -a FILENAME
+VALIDATE $? "staring mysql"
+
+mysql -h 54.235.31.231 -u root -pExpenseApp@1 | tee -a FILENAME
+
+if [ $? -ne 0 ]
+then
+    echo -e "$R mysql root password is setting $N" | tee -a FILENAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "setting mysql root password"
+else    
+    echo -e "$R mysql root password is alrdy set $N" | tee -a FILENAME
+fi
+
+
